7a611e7bd00d51d118ff7cd20fe30d486a11e86e
s3601030 <s3601030@student.rmit.edu.au>
2020-08-06 14:14:59 +1000

    Added springboot file system

---
+++ BackEnd/majorProject/src/test/java/com/rmit/sept/majorProject/MajorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.majorProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

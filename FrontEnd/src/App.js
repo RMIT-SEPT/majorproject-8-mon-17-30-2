39825776f9f604a5875e612a31b8b1c920b9b389
Joshua <s3687337@student.rmit.edu.au>
2020-08-16 19:18:43 +1000

    +safe merge

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login';
-
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import Register from "./components/Register";
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reloadtest.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    // <div className="App2">
+    //   <header className="App-header2">Create an Account</header>
+    //   <div>Create an Account</div>
+    //   <Register />
+    // </div>
+
     <div>
-        <Login/>
+      <Register />
     </div>
   );
 }

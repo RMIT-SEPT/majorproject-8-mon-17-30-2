e19259a879446bd76d496c9eafe04c17bbf67309
s3779726 <s3779726@student.rmit.edu.au>
2020-08-18 11:34:59 +1000

    Added Navbar routing to login and register pages

--- FrontEnd/src/components/Nav/Navbar.jsx
+++ FrontEnd/src/components/Nav/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //basic navbar with bootstrap
@@ -8,14 +9,26 @@ function Navbar(props){
         <a className = "navbar-brand nav-text" href= "/">AGME</a>
             <div className="navbar-collapse" id="navbarNav">
             <ul className = "navbar-nav">
-                <li className = "nav-item"><a className = "nav-link" href ="/">Home</a></li>   
-                <li className = "nav-item"><a className = "nav-link" href ="/">About</a></li>
-                <li className = "nav-item"><a className = "nav-link" href ="/">Contact</a></li>
+                <Link to = "/" className = "nav-link">
+                    <li className = "nav-item">Home</li>   
+                </Link>
+
+                <Link to = "/about" className = "nav-link">
+                    <li className = "nav-item">About</li>
+                </Link>
+
+                <Link to = "/contact" className = "nav-link">
+                    <li className = "nav-item">Contact</li>
+                </Link>
             </ul>
           
             <ul class="navbar-nav ml-auto">
-                <li className = "nav-item"><a className = "nav-link" href ="/">Login</a></li>
-                <li className = "nav-item"><a className = "nav-link" href ="/">Register</a></li>
+                <Link to = "/login" className = "nav-link">
+                    <li className = "nav-item">Login</li>
+                </Link>
+                <Link to = "/register" className = "nav-link">
+                    <li className = "nav-item">Register</li>
+                </Link>
             </ul>
          
             </div>

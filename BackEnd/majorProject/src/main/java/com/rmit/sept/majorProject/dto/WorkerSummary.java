3008db9a367f62b19ec99b31d352b9959d0b2a70
Joshua <s3687337@student.rmit.edu.au>
2020-08-21 13:12:13 +1000

    +controllers can now choose dto or raw object

--- BackEnd/majorProject/src/main/java/com/rmit/sept/majorProject/dto/WorkerSummary.java
+++ BackEnd/majorProject/src/main/java/com/rmit/sept/majorProject/dto/WorkerSummary.java
@@ -10,6 +10,8 @@ public class WorkerSummary {
 
     private Long id;
     private String name;
+    private String email;
+    private String username;
     private List<Long> services = new ArrayList<Long>();
     private List<Long> bookings = new ArrayList<Long>();
 
@@ -46,6 +48,22 @@ public class WorkerSummary {
 
     public Iterable<Long> getServices() {
         return this.services;        
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
 }

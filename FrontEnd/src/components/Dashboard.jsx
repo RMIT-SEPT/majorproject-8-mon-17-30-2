687036be5ad7a86e9d5417acee0397dcf921f71d
DANIEL ONG <s3676400@student.rmit.edu.au>
2020-08-28 14:38:52 +0800

    Customer Dashboard UI using cards

--- FrontEnd/src/components/Dashboard.jsx
+++ FrontEnd/src/components/Dashboard.jsx
@@ -1,17 +1,52 @@
 import React from "react";
 
-function Dashboard(props){
+function Dashboard(props) {
+  return (
+        <div className="container-fluid">
+        <div style={{display: 'flex', justifyContent: 'center'}}></div>
+          <h1 style={{display: 'flex', justifyContent: 'center'}}>{props.title}</h1>
+          <p style={{display: 'flex', justifyContent: 'center'}}> {props.desc}</p>
 
-    return(<div className = "container-fluid">
-        
-            <h1>{props.title}</h1>
-            <p>{props.desc}</p>
-        
-        </div>)
+          <div class="card-deck">
+            <div class="card text-center" >
+              <div class="card-body">
+                <h4 class="card-title">View Profile</h4>
+                <p class="card-text">
+                  <small class="text-muted">Edit your details!</small>
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Click Here!
+                </a>
+              </div>
+            </div>
 
+            <div class="card text-center">
+              <div class="card-body">
+                <h4 class="card-title">Make a Booking</h4>
+                <p class="card-text">
+                  <small class="text-muted">Make Bookings!</small>
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Click Here!
+                </a>
+              </div>
+            </div>
 
-
+            <div class="card text-center">
+              <div class="card-body">
+                <h4 class="card-title">Booking History</h4>
+                <p class="card-text">
+                  <small class="text-muted">View successfully completed bookings</small>
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Click Here!
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+   
+  );
 }
-
 
 export default Dashboard;

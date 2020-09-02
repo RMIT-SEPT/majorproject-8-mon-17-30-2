83be81dc4be9849968ea906d7585b070dc7d06cf
s3779726 <s3779726@student.rmit.edu.au>
2020-09-02 17:48:57 +1000

    fixed utils imports

--- FrontEnd/src/components/Login.js
+++ FrontEnd/src/components/Login.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "../css/Login.css";
-import "../utils/utils";
+import "../Utils/utils";
 import AuthenticationService from "../services/AuthenticationService";
-import { ID_SESSION_ATTRIBUTE, ROLE_SESSION_ATTRIBUTE } from "../utils/utils";
+import { ID_SESSION_ATTRIBUTE, ROLE_SESSION_ATTRIBUTE } from "../Utils/utils";
 
 //for debug printing using util2.inspect(object, false, null, true)
 const util2 = require('util');

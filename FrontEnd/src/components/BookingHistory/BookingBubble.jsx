af3596962f532c8f2c455b6d9f701bd278bc6a9e
Joshua <s3687337@student.rmit.edu.au>
2020-09-02 18:30:22 +1000

    +bookingbubble component skeleton

---
+++ FrontEnd/src/components/BookingHistory/BookingBubble.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+//this component is going to be a single grey bubble displaying the data of a single booking
+function BookingBubble(props) {
+    return <h1>test</h1>;
+}

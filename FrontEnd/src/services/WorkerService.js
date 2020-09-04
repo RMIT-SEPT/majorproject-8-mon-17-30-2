95479a2b9c0ad09678cde5de345803bceb6c6ea7
DANIEL ONG <s3676400@student.rmit.edu.au>
2020-09-04 11:38:25 +0800

    Created New WorkerService, modified BookingPage and AppRouting

---
+++ FrontEnd/src/services/WorkerService.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const WORKER_API_BASE_URL = 'http://localhost:8080/api/worker'
+
+class WorkerService {
+    
+    getAllWorkers(){
+        return axios.get(WORKER_API_BASE_URL);
+    }
+
+    getWorkerById(workerId){
+        return axios.get(WORKER_API_BASE_URL + '/' + workerId);
+    }
+
+
+    getWorkerByUsername(workerUsename){
+        return axios.get(WORKER_API_BASE_URL + '/' + workerUsename);
+    }
+
+  
+
+}
+
+export default new WorkerService()

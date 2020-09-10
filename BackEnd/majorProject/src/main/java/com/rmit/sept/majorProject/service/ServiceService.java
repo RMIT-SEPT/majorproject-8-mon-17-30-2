f9c426572b848c7eb270b534c8bbab076f0542ad
Joshua <s3687337@student.rmit.edu.au>
2020-09-10 17:18:52 +1000

    +removed the need for the service->job conversion

--- BackEnd/majorProject/src/main/java/com/rmit/sept/majorProject/service/ServiceService.java
+++ BackEnd/majorProject/src/main/java/com/rmit/sept/majorProject/service/ServiceService.java
@@ -1,14 +1,15 @@
 package com.rmit.sept.majorProject.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import com.rmit.sept.majorProject.dto.ServiceSummary;
+import com.rmit.sept.majorProject.model.Service;
 import com.rmit.sept.majorProject.model.Worker;
 import com.rmit.sept.majorProject.repository.ServiceRepository;
 import com.rmit.sept.majorProject.repository.WorkerRepository;
 
-@Service
+@org.springframework.stereotype.Service
 public class ServiceService{
 
 	@Autowired
@@ -16,24 +17,24 @@ public class ServiceService{
     @Autowired
     private WorkerRepository workerRepository;
 	
-	public Iterable<com.rmit.sept.majorProject.model.Service> findAll(){
+	public Iterable<Service> findAll(){
 		return repository.findAll();
 	}
 
-    public Iterable<com.rmit.sept.majorProject.model.Service> findByBusinessId(Long businessId){
+    public Iterable<Service> findByBusinessId(Long businessId){
         return repository.findByBusinessId(businessId);
     }
 
-    public Iterable<com.rmit.sept.majorProject.model.Service> findByWorkerId(Long workerId){
-        Iterable<com.rmit.sept.majorProject.model.Service> matchingServices;
+    public Iterable<Service> findByWorkerId(Long workerId){
+        Iterable<Service> matchingServices;
         Worker worker = workerRepository.findById(workerId).get();
-        matchingServices = worker != null ? worker.getServices() : new ArrayList<com.rmit.sept.majorProject.model.Service>();
+        matchingServices = worker != null ? worker.getServices() : new ArrayList<Service>();
         return matchingServices;
     }
     
     public Iterable<ServiceSummary> findByWorkerIdDTO(Long workerId){
         ArrayList<ServiceSummary> serviceDtos = new ArrayList<ServiceSummary>();
-        for(com.rmit.sept.majorProject.model.Service service : findByWorkerId(workerId)){
+        for(Service service : findByWorkerId(workerId)){
             serviceDtos.add(new ServiceSummary(service));
         }
         return serviceDtos;

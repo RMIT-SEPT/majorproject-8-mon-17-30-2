53ceb5d664b4d5e2bd1c71731d631474bd03f615
s3779726 <s3779726@student.rmit.edu.au>
2020-09-12 15:31:03 +1000

    Complete past booking summary Page

--- FrontEnd/src/components/AdminBookingSummary/PastBookingSummary.jsx
+++ FrontEnd/src/components/AdminBookingSummary/PastBookingSummary.jsx
@@ -2,34 +2,31 @@ import React, {useState, useEffect} from "react";
 import BusinessService from "../../services/BusinessService";
 import AdminService from "../../services/AdminService";
 import AuthenticationService from "../../services/AuthenticationService";
+import BookingBubble from "../BookingHistory/BookingBubble";
 
 function PastBookingSummary(props){
 
-    const [business, setBusiness] = useState();
     const [bookings, setBookings] = useState([]);
 
     useEffect(() => {
         AdminService.getAdminById(AuthenticationService.getLoggedInId()).then(response => {
-            setBusiness(response.data.businessId);
-        }).catch((response) => console.log(response));
-
-
-
-
-        BusinessService.getBusinessPastBookings(business).then(response => {
-            console.log(response.data);
-            setBookings(response.data)
-        }).catch(console.log("ERROR OBTAINING PAST BOOKINGS"));
-
-
-        
+            let businessId = response.data.businessId;
+            getPastBookings(businessId);
+        }).catch();
     }, []);
+
+    function getPastBookings(businessId){
+        BusinessService.getBusinessPastBookings(businessId).then(response => {
+            setBookings(response.data)
+        }).catch();
+
+    }
 
     return(
         <div>
             <header className="bookings-header"><span role="img" aria-label="Time emoji">🕒</span> {props.title} <span role="img" aria-label="Time emoji">🕒</span></header>
             { bookings.length > 0 ? 
-                <h1> booking found </h1>
+               bookings.map((booking) => <BookingBubble key = {booking.id} booking = {booking} />)
                 :
                 <div className = "bookings-header">No Bookings Found</div>
             }

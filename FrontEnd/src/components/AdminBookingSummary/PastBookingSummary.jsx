66d143448850db449a6b2bf4f7afa7fdd26e8554
Joshua <s3687337@student.rmit.edu.au>
2020-10-03 23:23:43 +1000

    +table added, sorted by date

--- FrontEnd/src/components/AdminBookingSummary/PastBookingSummary.jsx
+++ FrontEnd/src/components/AdminBookingSummary/PastBookingSummary.jsx
@@ -3,10 +3,48 @@ import BusinessService from "../../services/BusinessService";
 import AdminService from "../../services/AdminService";
 import AuthenticationService from "../../services/AuthenticationService";
 import BookingBubble from "../Bubbles/BookingBubble";
+import BootstrapTable from 'react-bootstrap-table-next';
 
 function PastBookingSummary(props){
 
     const [bookings, setBookings] = useState([]);
+    const columns = [{
+        dataField: 'id',
+        text: 'ID',
+        sort: true
+    }, {
+        dataField: 'businessName',
+        text: 'Business',
+        sort: true
+    }, {
+        dataField: 'date',
+        text: 'Date',
+        sort: "desc"
+    }, {
+        dataField: 'startTime',
+        text: 'Start Time',
+        sort: true
+    }, {
+        dataField: 'endTime',
+        text: 'End Time',
+        sort: true
+    }, {
+        dataField: 'serviceTitle',
+        text: 'Service',
+        sort: true
+    }, {
+        dataField: 'workerName',
+        text: 'Worker',
+        sort: true
+    }, {
+        dataField: 'customerName',
+        text: 'Customer',
+        sort: true
+    }]
+    const style = {
+        maxWidth: "50%",
+        margin: "auto"
+    }
 
     useEffect(() => {
         AdminService.getAdminById(AuthenticationService.getLoggedInId()).then(response => {
@@ -22,14 +60,10 @@ function PastBookingSummary(props){
     }
 
     return(
-        <div>
+        
+        <div style={style}>
             <header className="bookings-header"><span role="img" aria-label="Time emoji">🕒</span> {props.title} <span role="img" aria-label="Time emoji">🕒</span></header>
-            { bookings.length > 0 ? 
-               bookings.map((booking) => <BookingBubble key = {booking.id} booking = {booking} />)
-                :
-                <div className = "bookings-header">No Bookings Found</div>
-            }
-
+            <BootstrapTable sort={{dataField: 'date', order: 'asc'}} keyField='id' data={bookings} columns={columns} />
         </div>
     );
 }

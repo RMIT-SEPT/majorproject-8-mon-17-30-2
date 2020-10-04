344ac55666342fb55b8996f4f3cd31780d591e8e
Daniel Ong <s3676400@student.rmit.edu.au>
2020-10-04 22:56:53 +0800

    Front end in the works

--- FrontEnd/src/components/Business/ViewAvailability.jsx
+++ FrontEnd/src/components/Business/ViewAvailability.jsx
@@ -1,79 +1,38 @@
-// import React, {Component} from "react";
-// import BookingBubble from "../Bubbles/BookingBubble.jsx";
-// import BusinessService from "../../services/BusinessService";
-// import AuthenticationService from "../../services/AuthenticationService";
-
-// //for debug printing using util2.inspect(object, false, null, true)
-// // const util2 = require('util');
-
-// class ViewAvailabilty extends Component {
-
-//   state = {
-//     bookings: []
-//   }
-
-  
-//   componentDidMount() {
-//     BusinessService.getBusinessAvailability(AuthenticationService.getBusinessId()).then((response) => {
-//       this.setState({
-//         bookings: response.data
-//       })
-//     });
-//   }
-
-//   render() {
-//     // let bookings = <div className = "bookings-header">No Bookings Found</div>
-//     // if(this.state.bookings.length > 0){
-//     //   bookings = this.state.bookings.map((booking) => {
-//     //     return <BookingBubble key = {booking.id} booking = {booking} />
-//     //   });
-//     // }
-
-//     return (
-//       <div>
-//         {/* <header className="bookings-header"><span role="img" aria-label="Time emoji">🕒</span> Booking History <span role="img" aria-label="Time emoji">🕒</span></header>
-//         {bookings} */}
-//       </div>
-
-//     );
-//   }
-
-// }
-
-// export default ViewAvailabilty;
-
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import BusinessService from "../../services/BusinessService";
 import AdminService from "../../services/AdminService";
 import AuthenticationService from "../../services/AuthenticationService";
 import BookingBubble from "../Bubbles/BookingBubble";
 
-function ViewAvailabilty(props){
+function ViewAvailabilty(props) {
 
     const [availSlots, setAvailSlots] = useState([]);
 
     useEffect(() => {
         AdminService.getAdminById(AuthenticationService.getLoggedInId()).then(response => {
-            // let businessId = response.data.businessId;
             getAvailiableWorkSlots();
         }).catch();
     }, []);
 
-    function getAvailiableWorkSlots(){
+    function getAvailiableWorkSlots() {
         BusinessService.getBusinessAvailability(AuthenticationService.getBusinessId()).then(response => {
             setAvailSlots(response.data)
         }).catch();
     }
 
-    return(
+    return (
         <div>
-            <header className="bookings-header"><span role="img" aria-label="Time emoji">Available WorkSlot</span> {props.title} <span role="img" aria-label="Time emoji">🕒</span></header>
-            { availSlots.length > 0 ? 
+            <header className="bookings-header"><span role="img" aria-label="Time emoji">All Available Worker Slots</span> </header>
+            {/* { availSlots.length > 0 ?
                availSlots.map((booking) => <BookingBubble key = {booking.id} booking = {booking} />)
                 :
                 <div className = "bookings-header">No Bookings Found</div>
-            }
-
+            } */}
+            <ul>
+                {availSlots.map(availSlots => (
+                    <div key={availSlots.id}>{availSlots.businessName}{availSlots.date}</div>
+                ))}
+            </ul>
         </div>
     );
 }
